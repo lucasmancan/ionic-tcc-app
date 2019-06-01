02ee159b7411c104662b8b7cf2f4a3273ef0de35
unknown <pedrofarbo@gmail.com>
2019-05-31 23:01:05 -0300

    melhorias de interface

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,8 @@
 import { ModalController } from '@ionic/angular';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OptionsPage } from '../options/options.page';
 import { OverlayEventDetail } from '@ionic/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -10,25 +11,32 @@ import { OverlayEventDetail } from '@ionic/core';
 })
 export class TabsPage {
 
-constructor(private modalController: ModalController){}
+  constructor(private modalController: ModalController, public router: Router) { }
+
+  public routerName: any;
 
   async openModal() {
     const modal: HTMLIonModalElement =
-       await this.modalController.create({
-          component: OptionsPage
-          // componentProps: {
-          //    aParameter: true,
-          //    otherParameter: new Date()
-          // }
-    });
-     
+      await this.modalController.create({
+        component: OptionsPage
+        // componentProps: {
+        //    aParameter: true,
+        //    otherParameter: new Date()
+        // }
+      });
+
     modal.onDidDismiss().then((detail: OverlayEventDetail) => {
-       if (detail !== null) {
-         console.log('The result:', detail.data);
-       }
+      if (detail !== null) {
+        console.log('The result:', detail.data);
+      }
     });
-    
+
     await modal.present();
-}
+  }
+
+  verifyRoute(name: any) {
+      this.routerName = name;
+      console.log(this.routerName);
+  }
 
 }
